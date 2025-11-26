154dd324aa75f6a6f1255111517c5dd905f1cef7
Maya Ventura <maya@Hillcrest.local>
2025-11-26 00:33:36 -0500

    tweak config

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,20 @@ dotenv.config();
 // EDIT THIS!
 function postTextFromImageName(imageName: string): string {
   // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
-  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
+  //const dateParts = imageName.replace('.jpg', '').split('-');
+  //const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
 
   // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+  //const formatter = new Intl.DateTimeFormat('en-US', {
+    //year: 'numeric',
+    //month: 'long',
+    //day: 'numeric',
+  //});
 
   // Format the date
-  return formatter.format(date);
-}
+  //return formatter.format(date);
+  return 'i dont need this';
+//}
 
 // EDIT THIS!
 function altTextFromImageName(imageName: string): string {
