14df31d8f61347a428100f103f381ea9cace62b9
Maya Ventura <maya@Hillcrest.local>
2025-11-26 02:08:01 -0500

    change typo from forked code

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,14 @@
 import { postImage } from './clients/at';
-import { getNextImage } from './images';
+import { getNextImage } from './images'; 
 import * as dotenv from 'dotenv';
 dotenv.config();
-
-// EDIT THIS!
-//function postTextFromImageName(imageName: string): string {
-  // Remove the file extension and parse the date
-//   const dateParts = imageName.replace('.jpg', '').split('-');
-//   const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
-
-//   // Create a formatter
-//   const formatter = new Intl.DateTimeFormat('en-US', {
-//     year: 'numeric',
-//     month: 'long',
-//     day: 'numeric',
-//   });
-
-//   // Format the date
-//   return formatter.format(date);
-//}
 
 // EDIT THIS!
 function altTextFromFilename(filename: string): string {
   return 'Peanuts: ' + (filename);
 }
-
-// Shouldn't have to edit this.
 async function main() {
-  const { LAST_IMAGE_NAME: lastImageName } = process.env;
-  const nextImage = await getNextImage({ lastImageName });
+  const nextImage = await getRandomImage(); 
 
   console.log(nextImage.imageName);
 
